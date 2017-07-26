51554a7196927ed1de29e07c69b4e58722eb09b4
Dimitris Baltas <dibaltas@gmail.com>
2017-07-26 17:49:45 +0200

    Adding Authentication and ContactWebContext migrations.

--- ContactWeb/Controllers/ContactsController.cs
+++ ContactWeb/Controllers/ContactsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ContactWeb.Models;
+using Microsoft.AspNet.Identity;
 
 namespace ContactWeb.Controllers
 {
@@ -15,12 +16,31 @@ namespace ContactWeb.Controllers
         private ContactWebContext db = new ContactWebContext();
 
         // GET: Contacts
+        [Authorize]
         public ActionResult Index()
         {
-            return View(db.Contacts.ToList());
+            var userId = GetCurrentUserId();
+            /* try
+            {
+                
+                //var userName = User.Identity.GetUserName();
+
+                /*ViewBag.UserId = userId;
+                ViewBag.UserName = userName;
+
+                ViewData["UserId"] = userId;
+                ViewData["UserName"] = userName;
+            }
+            catch (System.Exception ex)
+            {
+                throw;
+            } */
+           
+            return View(db.Contacts.Where(x => x.UserId == userId).ToList());
         }
 
         // GET: Contacts/Details/5
+        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,6 +56,7 @@ namespace ContactWeb.Controllers
         }
 
         // GET: Contacts/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -46,6 +67,7 @@ namespace ContactWeb.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Create([Bind(Include = "Id,UserId,FirstName,LastName,Email,PhonePrimary,PhoneSecondary,Birthday,StreetAddress1,StreetAddress2,City,State,Zip")] Contact contact)
         {
             if (ModelState.IsValid)
@@ -59,6 +81,7 @@ namespace ContactWeb.Controllers
         }
 
         // GET: Contacts/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -78,6 +101,7 @@ namespace ContactWeb.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Edit([Bind(Include = "Id,UserId,FirstName,LastName,Email,PhonePrimary,PhoneSecondary,Birthday,StreetAddress1,StreetAddress2,City,State,Zip")] Contact contact)
         {
             if (ModelState.IsValid)
@@ -90,6 +114,7 @@ namespace ContactWeb.Controllers
         }
 
         // GET: Contacts/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -107,6 +132,7 @@ namespace ContactWeb.Controllers
         // POST: Contacts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult DeleteConfirmed(int id)
         {
             Contact contact = db.Contacts.Find(id);
@@ -122,6 +148,16 @@ namespace ContactWeb.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public Guid GetCurrentUserId()
+        {
+            return new Guid(User.Identity.GetUserId());
+        }
+
+        private bool EnsureIsUserContact(Contact contact)
+        {
+            return contact.UserId == GetCurrentUserId();
         }
     }
 }
--- ContactWeb/Migrations/Configuration.cs
+++ ContactWeb/Migrations/Configuration.cs
@@ -5,15 +5,19 @@ namespace ContactWeb.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ContactWeb.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ContactWeb.Models.ContactWebContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            ContextKey = "ContactWeb.Models.ContactWebContext";
         }
 
-        protected override void Seed(ContactWeb.Models.ApplicationDbContext context)
+        protected override void Seed(ContactWeb.Models.ContactWebContext context)
         {
+            /* 05917237-0235-49a6-9913-17ec97f4ab5c
+             * 2eca5b3e-3fa6-4232-810b-5bfb2b94d849
+             */
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -26,6 +30,94 @@ namespace ContactWeb.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Contacts.AddOrUpdate(
+                p => p.Id,
+                new Models.Contact
+                {
+                    Id = 1,
+                    Birthday = new DateTime(1920, 01, 20),
+                    City = "Chicago"
+                                    ,
+                    Email = "doc.mccoy@starfleet.com",
+                    FirstName = "DeForest",
+                    LastName = "Kelley"
+                                    ,
+                    PhonePrimary = "123-456-7890",
+                    PhoneSecondary = "234-567-8901",
+                    State = "IL"
+                                    ,
+                    StreetAddress1 = "Sickbay",
+                    StreetAddress2 = "Starship Enterprise NCC-1701"
+                                    ,
+                    UserId = new Guid("05917237-0235-49a6-9913-17ec97f4ab5c")
+                                    ,
+                    Zip = "98765"
+                }
+                , new Models.Contact
+                {
+                    Id = 2,
+                    Birthday = new DateTime(1920, 03, 03),
+                    City = "New York"
+                                    ,
+                    Email = "i.beam.you.up@starfleet.com",
+                    FirstName = "James",
+                    LastName = "Doohan"
+                                    ,
+                    PhonePrimary = "345-678-9012",
+                    PhoneSecondary = "456-789-0123",
+                    State = "NY"
+                                    ,
+                    StreetAddress1 = "Engineering",
+                    StreetAddress2 = "Starship Enterprise NCC-1701"
+                                    ,
+                    UserId = new Guid("05917237-0235-49a6-9913-17ec97f4ab5c")
+                                    ,
+                    Zip = "87654"
+                }
+                , new Models.Contact
+                {
+                    Id = 3,
+                    Birthday = new DateTime(1931, 03, 26),
+                    City = "Los Angeles"
+                                    ,
+                    Email = "its.only.logic@starfleet.com",
+                    FirstName = "Leonard",
+                    LastName = "Nimoy"
+                                    ,
+                    PhonePrimary = "987-654-3210",
+                    PhoneSecondary = "876-543-2109",
+                    State = "CA"
+                                    ,
+                    StreetAddress1 = "Science Station 1",
+                    StreetAddress2 = "Starship Enterprise NCC-1701"
+                                    ,
+                    UserId = new Guid("2eca5b3e-3fa6-4232-810b-5bfb2b94d849")
+                                    ,
+                    Zip = "76543-2109"
+                }
+                , new Models.Contact
+                {
+                    Id = 4,
+                    Birthday = new DateTime(1931, 03, 22),
+                    City = "Riverside"
+                                    ,
+                    Email = "the.captain@starfleet.com",
+                    FirstName = "William",
+                    LastName = "Shatner"
+                                    ,
+                    PhonePrimary = "765-432-1098",
+                    PhoneSecondary = "654-321-0987",
+                    State = "IA"
+                                    ,
+                    StreetAddress1 = "The Bridge",
+                    StreetAddress2 = "Starship Enterprise NCC-1701"
+                                    ,
+                    UserId = new Guid("2eca5b3e-3fa6-4232-810b-5bfb2b94d849")
+                                    ,
+                    Zip = "65432-0123"
+                }
+            );
         }
     }
 }
